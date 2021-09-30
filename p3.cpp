219b74b1f3409f251e5279776f68c7b14d128960
Sagar Kamdi <75541396+sagar-kamdi@users.noreply.github.com>
2021-09-30 16:32:10 +0530

    Create p3.cpp

---
+++ p3.cpp
@@ -0,0 +1,25 @@
+/*
+55555
+4444
+333
+22
+1
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    for (int i = 5; i >= 1; i--) 
+    {
+        for (int j = 1; j <=i; j++) 
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
